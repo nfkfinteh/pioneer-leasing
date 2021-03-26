4a844e17e049fb8a88d81a9f547f3aa760b80dfd
root <solovyow_iv@nfksber.ru>
2021-03-26 12:20:31 +0300

    стили таблицы для контента новостей

--- local/templates/pioner-lizing/include_areas/contacts/form_phone.php
+++ local/templates/pioner-lizing/include_areas/contacts/form_phone.php
@@ -1,2 +1,2 @@
-<a href="tel:+78352458989">+7 (8352) <span class="h3">45-89-89</span></a><span
-										class="h3">, </span><a href="tel:+78352454560"><span class="h3">45-45-60</span></a>
+<a href="tel:+78352458989">+7 (8352) <span class="h3">45-89-89</span></a><span class="h3">, </span><a href="tel:+78352454560"><span class="h3">45-45-60</span></a><br>
+<a href="tel:88003003939">8 (800) <span class="h3">300-39-39</span></a>
--- local/templates/pioner-lizing/include_areas/contacts/phone_footer.php
+++ local/templates/pioner-lizing/include_areas/contacts/phone_footer.php
@@ -1,1 +1,3 @@
 +7 (8352) <a class="footer__phone" href="tel:+78352458989">45-89-89</a>, <a class="footer__phone" href="tel:+78352454560">45-45-60</a>
+<br>
+8 (800) <a href="tel:88003003939" footer__phone>300-39-39</a>
